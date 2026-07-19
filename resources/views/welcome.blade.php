cea8367a1eeb7a7332775806ecf24f666a1d697b
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-19 10:37:27 +0545

    welcome.bladeorabout.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Tailwind CSS -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+  <h1 class="text-4xl font-semibold"> Home Page </h1>
+</body>
+</html>
